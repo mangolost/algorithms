9eccc8bb165fa96c941bdccd25fd7613bf9161f6
chen.li200李晨 <chen.li200@chinaredstar.com>
2020-07-29 15:42:41 +0800

    first commit

---
+++ src/main/java/com/mangolost/algorithms/sorting/SortMethod.java
@@ -0,0 +1,14 @@
+package com.mangolost.algorithms.sorting;
+
+/**
+ *
+ * @param <T>
+ */
+public interface SortMethod<T> {
+
+    /**
+     *
+     * @param a
+     */
+    void sort(Comparable<T>[] a);
+}
